62d72731140b77c6522753811cb1b6ad2bfe5777
mondeer <keringlaban@yahoo.com>
2017-09-28 13:06:41 +0300

    added truck.png to home view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,8 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  items: any = new Array(10);
+  
   constructor(public navCtrl: NavController) {
 
   }
