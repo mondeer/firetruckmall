857037d5f1978984f6b35435b1cd0f79038a1ed6
mondeer <keringlaban@yahoo.com>
2017-10-13 16:23:21 +0300

    removed fingerprintaio

--- plugins/cordova-plugin-fingerprint-aio/tests/tests.js
+++
@@ -1,7 +0,0 @@
-exports.defineAutoTests = function() {
-  describe('Fingerprint Object', function () {
-    it("should exist", function() {
-      expect(window.Fingperprint).toBeDefined();
-    });
-  });
-};
